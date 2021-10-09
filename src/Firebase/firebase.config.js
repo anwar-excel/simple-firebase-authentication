35048cf1e09e834a91053db18d9f32f2ff584ad9
unknown <anwarhosen575@gmail.com>
2021-10-09 17:23:29 +0600

    google-sign-in

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA4t7SRpYlKlYnpsfwJbySSgbQr31WUtug",
+    authDomain: "simple-firebase-authenti-bec33.firebaseapp.com",
+    projectId: "simple-firebase-authenti-bec33",
+    storageBucket: "simple-firebase-authenti-bec33.appspot.com",
+    messagingSenderId: "702478233502",
+    appId: "1:702478233502:web:e852adb565a16ea40d708f"
+};
+
+export default firebaseConfig;
